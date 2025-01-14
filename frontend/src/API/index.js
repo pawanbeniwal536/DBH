66f9b0286551da38efba60df05447819deee016e
pawanbeniwal536 <152151615+pawanbeniwal536@users.noreply.github.com>
2025-01-14 21:32:27 +0530

    Update index.js for connect to server

--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000", // Replace with your server URL
+    baseURL: "https://dbh-edcd.onrender.com", // Replace with your server URL
 });
 
 export default api;
